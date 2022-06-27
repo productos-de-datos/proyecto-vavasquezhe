fe6f1c1a3a1b4aad5ea38a051768c06a8aea6073
Valentina Vásquez Hernandez <vavasquezhe@unal.edu.co>
2022-06-27 16:53:26 -0500

    updating dependencies

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -28,7 +28,7 @@ def make_forecasts():
     parent_dir = str(get_project_root())
     #raise NotImplementedError("Implementar esta función")
 
-    df = pd.read_csv(parent_dir + '/data_lake/business/features/precios-diarios.csv')
+    df = pd.read_csv(parent_dir + '/data_lake/business/features/precios_diarios.csv')
     df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
     df = df.set_index('fecha')
     df = df.asfreq('D')
