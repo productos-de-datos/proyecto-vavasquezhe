aa4a45b4cfbf388ee97bc4408e708b80b52ebcd4
Valentina Vásquez Hernandez <vavasquezhe@unal.edu.co>
2022-06-27 14:04:25 -0500

    adding train forecast

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -1,3 +1,13 @@
+def load_estimator():
+
+    import os
+    import pickle
+
+    with open("precios-diarios.pkl", "rb") as file:
+        estimator = pickle.load(file)
+
+    return estimator    
+    
 def make_forecasts():
     """Construya los pronosticos con el modelo entrenado final.
 
@@ -12,10 +22,30 @@ def make_forecasts():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    ##raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+    from train_daily_model import test_train_datasets, get_project_root 
+    parent_dir = str(get_project_root())
+    #raise NotImplementedError("Implementar esta función")
 
+    df = pd.read_csv(parent_dir + '/data_lake/business/features/precios-diarios.csv')
+    df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
+    df = df.set_index('fecha')
+    df = df.asfreq('D')
+    df = df.sort_index()
+
+    data_train, data_test = test_train_datasets(df,0.25)
+    price_estimator = load_estimator()
+    steps = len(data_test)
+    predictions = price_estimator.predict(steps=steps)
+    
+    result = pd.concat([data_test.loc[:, ['precio']], predictions], axis=1, join="inner")
+    result = result.reset_index()
+    result.columns = ["fecha", "precio", "precio_pred"]
+
+    result.to_csv(parent_dir+'/data_lake/business/forecasts/precios-diarios.csv',index=False)
 
 if __name__ == "__main__":
     import doctest
-
+    make_forecasts()    
     doctest.testmod()
--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -1,3 +1,19 @@
+def get_project_root():
+    from pathlib import Path
+    return Path(__file__).parent.parent.parent
+
+def test_train_datasets(data,perc):
+    n = round(len(data)*perc)
+    data_train = data[:-n]
+    data_test  = data[-n:]
+    return data_train, data_test
+
+def save_estimator(estimator):
+    import os
+    import pickle
+    with open("precios-diarios.pkl", "wb") as file:
+        pickle.dump(estimator, file)
+
 def train_daily_model():
     """Entrena el modelo de pronóstico de precios diarios.
 
@@ -6,10 +22,33 @@ def train_daily_model():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+
+    import pandas as pd
+    from sklearn.ensemble import RandomForestRegressor
+    from skforecast.ForecasterAutoreg import ForecasterAutoreg
+    parent_dir = str(get_project_root())
+    #raise NotImplementedError("Implementar esta función")
+
+    df = pd.read_csv(parent_dir + '/data_lake/business/features/precios-diarios.csv')
+    df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
+    df = df.set_index('fecha')
+    df = df.asfreq('D')
+    df = df.sort_index()
+
+    data_train, data_test = test_train_datasets(df,0.25)
+
+    forecaster = ForecasterAutoreg(
+                regressor = RandomForestRegressor(random_state=123),
+                lags = 8
+                )
+
+    forecaster.fit(y=data_train['precio'])
+
+    save_estimator(forecaster)
+    
 
 
 if __name__ == "__main__":
     import doctest
-
+    train_daily_model()
     doctest.testmod()
